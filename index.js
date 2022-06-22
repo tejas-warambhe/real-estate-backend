57b0b9f854b0ff8fd81a277d3db0ef0fafa10749
Tejas <tejaswarambhe@gmail.com>
2022-06-23 02:02:47 +0530

    File upload using GridFsBucket

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const connectToMongo = require('./db');
+const connection = require('./db');
 const user = require('./routes/user');
+const mongoose = require('mongoose');
+const Grid = require("gridfs-stream");
 
 //connect to database
-connectToMongo();
+let gfs, gridfsBucket;
+connection();
+
+const conn = mongoose.connection;
+conn.once("open", function() {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: "photos"
+    });
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection("photos");
+});
 
 //middlewares
 app.use(cors());
@@ -15,6 +27,33 @@ app.use(express.json());
 
 //specifying routes
 app.use('/user', user);
+
+
+// media routes
+app.get("/user/:filename", async(req, res) => {
+
+    try {
+        const file = await gfs.files.findOne({ filename: req.params.filename });
+        // console.log(file);
+        const readStream = gridfsBucket.openDownloadStream(file._id);
+        readStream.pipe(res);
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.delete("/user/:filename", async(req, res) => {
+    try {
+        await gfs.files.deleteOne({ filename: req.params.filename });
+        res.send("success");
+    } catch (error) {
+        console.log(error);
+        res.send("An error occured.");
+    }
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log("Listening on Port " + PORT)
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Enquiry = require('../models/Enquiry');
+const upload = require('../middlewares/upload');
+
+
 
 router.get('/ping', (req, res) => {
     res.status(201).json({ success: "Pong" });
 })
+
+router.post("/upload", upload.single("file"), async(req, res) => {
+    if (req.file === undefined) return res.send("you must select a file.");
+    const imgUrl = `http://localhost:5000/user/${req.file.filename}`;
+    return res.send(imgUrl);
+});
+
+
+
 
 
 router.post('/enquiry', async(req, res) => {
